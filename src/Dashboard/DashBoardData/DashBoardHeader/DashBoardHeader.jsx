c4a14c7812bf0fa0e3a40bd8807563eddb027a38
Aashutosh <aashiiraj7770@gmail.com>
2025-12-06 18:05:01 +0530

    Set notification click and hover

--- src/Dashboard/DashBoardData/DashBoardHeader/DashBoardHeader.jsx
+++ src/Dashboard/DashBoardData/DashBoardHeader/DashBoardHeader.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Notification, SearchIcon } from "../../../assets/Icons/Invoice";
 import UserProfile from "./UserProfile";
-import logoMobile from "../../../assets/Images/logoGrab.png"
-const DashBoardHeader = ({setIsOpen}) => {
+import logoMobile from "../../../assets/Images/logoGrab.png";
+const DashBoardHeader = ({ setIsOpen }) => {
+  const [showNotification, setShowNotification] = useState(false);
 
+  const handleNotification = () => {
+    setShowNotification((prev) => !prev);
+  };
   const notificationList = [
     {
       id: 1,
@@ -21,12 +25,17 @@ const DashBoardHeader = ({setIsOpen}) => {
   return (
     <>
       <div className="flex  justify-between items-center gap-5 border-b border-[#F3F5F7] py-[18px] px-8 bg-white">
-        <button className="block sm:hidden text-[#596780] cursor-pointer" onClick={()=>{setIsOpen(prev=>!prev)}}>
+        <button
+          className="block sm:hidden text-[#596780] cursor-pointer"
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+        >
           ☰
         </button>
-          <div className="block sm:hidden">
-            <img src={logoMobile} alt=""></img>
-          </div>
+        <div className="block sm:hidden">
+          <img src={logoMobile} alt=""></img>
+        </div>
         <div className="hidden sm:block grow-0 shrink-0 basis-1/2 relative max-w-200px lg:max-w-[492px] w-full">
           <input
             type="search"
@@ -36,8 +45,12 @@ const DashBoardHeader = ({setIsOpen}) => {
           <SearchIcon className="absolute top-1/2 left-5 -translate-y-1/2 text-[#596780]" />
         </div>
         <div className="grow-0 shrink-0 basis-1/2 flex justify-end items-center gap-[32px]">
-          <div className="group border border-[#F3F5F7] min-w-[44px] min-h-[44px] rounded-full flex justify-center items-center cursor-pointer relative">
+          <div
+            onClick={handleNotification}
+            className="group border border-[#F3F5F7] min-w-[44px] min-h-[44px] rounded-full flex justify-center items-center cursor-pointer relative"
+          >
             <Notification className="text-[#596780]" />
+
             {notificationSign > 0 && (
               <>
                 {" "}
@@ -48,7 +61,12 @@ const DashBoardHeader = ({setIsOpen}) => {
             )}
 
             {notificationSign === true && (
-              <div className="absolute top-10 right-0 bg-white w-[240px] shadow-lg rounded-md  hidden group-hover:block">
+              <div
+                className={`absolute top-10 right-0 bg-white w-[240px] shadow-lg rounded-md 
+               hidden group-hover:block 
+                  ${showNotification ? "!block" : ""}            
+               `}
+              >
                 {notificationList.map((items, index) => {
                   return (
                     <div className="p-4 hover:bg-[#f2f2f2]" key={index}>
@@ -60,7 +78,7 @@ const DashBoardHeader = ({setIsOpen}) => {
             )}
           </div>
 
-          <UserProfile/>
+          <UserProfile />
         </div>
       </div>
     </>
